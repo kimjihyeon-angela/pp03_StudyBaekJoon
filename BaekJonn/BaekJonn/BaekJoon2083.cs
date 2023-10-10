05c2d20271580452fc5ccca1ef4bd9967aac4a7d
kimjihyeon-angela <angela9830@naver.com>
2023-10-10 15:09:41 +0900

    2023.10.06.

---
+++ BaekJonn/BaekJonn/BaekJoon2083.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaekJonn
+{
+    public partial class BaekJoon2083 : Form
+    {
+        public BaekJoon2083()
+        {
+            InitializeComponent();
+        }
+    }
+}
