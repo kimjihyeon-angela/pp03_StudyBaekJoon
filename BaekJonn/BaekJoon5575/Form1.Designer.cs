ce43fd1ad3e98cd18845403aef46e3f842775c34
kjh <angela9830@naver.com>
2023-10-18 13:39:31 +0900

    2023.10.18.

---
+++ BaekJonn/BaekJoon5575/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace BaekJoon5575
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxA = new System.Windows.Forms.TextBox();
+            this.tbxB = new System.Windows.Forms.TextBox();
+            this.tbxC = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "B :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "C :";
+            // 
+            // tbxA
+            // 
+            this.tbxA.Location = new System.Drawing.Point(52, 20);
+            this.tbxA.Name = "tbxA";
+            this.tbxA.Size = new System.Drawing.Size(216, 21);
+            this.tbxA.TabIndex = 3;
+            // 
+            // tbxB
+            // 
+            this.tbxB.Location = new System.Drawing.Point(52, 47);
+            this.tbxB.Name = "tbxB";
+            this.tbxB.Size = new System.Drawing.Size(216, 21);
+            this.tbxB.TabIndex = 4;
+            // 
+            // tbxC
+            // 
+            this.tbxC.Location = new System.Drawing.Point(52, 74);
+            this.tbxC.Name = "tbxC";
+            this.tbxC.Size = new System.Drawing.Size(216, 21);
+            this.tbxC.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(288, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(384, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "C :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(385, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "B :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(384, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 12);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "A :";
+            // 
+            // lblC
+            // 
+            this.lblC.Location = new System.Drawing.Point(411, 77);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(101, 12);
+            this.lblC.TabIndex = 12;
+            this.lblC.Text = "C :";
+            // 
+            // lblB
+            // 
+            this.lblB.Location = new System.Drawing.Point(412, 50);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(100, 12);
+            this.lblB.TabIndex = 11;
+            this.lblB.Text = "B :";
+            // 
+            // lblA
+            // 
+            this.lblA.Location = new System.Drawing.Point(411, 23);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(101, 12);
+            this.lblA.TabIndex = 10;
+            this.lblA.Text = "A :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 113);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbxC);
+            this.Controls.Add(this.tbxB);
+            this.Controls.Add(this.tbxA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxA;
+        private System.Windows.Forms.TextBox tbxB;
+        private System.Windows.Forms.TextBox tbxC;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblA;
+    }
+}
+
---
+++ BaekJonn/BaekJoon5575/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaekJoon5575
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string getA = tbxA.Text;
+            string[] A = getA.Split(' ');
+
+            int[] timeA = A
+                .Select(str => int.Parse(str))
+                .ToArray();
+
+            for (int i = 0; i < timeA.Length; i++)
+            {
+                Debug.WriteLine(timeA[i].ToString());
+                int h1 = timeA[0];
+                int m1 = timeA[1];
+                int s1 = timeA[2];
+                int h2 = timeA[3];
+                int m2 = timeA[4];
+                int s2 = timeA[5];
+
+                int s = 0;
+                int m = 0;
+                int h = 0;
+
+                if (s2 - s1 <0)
+                {
+                    s = s2 - s1 + 60;
+                    m2 -= 1;
+                }
+
+                if (m2 - m1 < 0)
+                {
+                    m = m2 - m1 + 60;
+                    h2 -= 1;
+                }
+
+                h = h2 - h1;
+
+                lblA.Text = h.ToString() + ' ' + m.ToString() + ' ' + s.ToString();
+            }
+
+            string getB = tbxB.Text;
+            string[] B = getB.Split(' ');
+
+            int[] timeB = B
+                .Select(str => int.Parse(str))
+                .ToArray();
+
+            for (int i = 0; i < timeB.Length; i++)
+            {
+                Debug.WriteLine(timeB[i].ToString());
+                int h1 = timeB[0];
+                int m1 = timeB[1];
+                int s1 = timeB[2];
+                int h2 = timeB[3];
+                int m2 = timeB[4];
+                int s2 = timeB[5];
+
+                int s = 0;
+                int m = 0;
+                int h = 0;
+
+                if (s2 - s1 < 0)
+                {
+                    s = s2 - s1 + 60;
+                    m2 -= 1;
+                }
+
+                if (m2 - m1 < 0)
+                {
+                    m = m2 - m1 + 60;
+                    h2 -= 1;
+                }
+
+                h = h2 - h1;
+
+                lblB.Text = h.ToString() + ' ' + m.ToString() + ' ' + s.ToString();
+            }
+
+            string getC = tbxC.Text;
+            string[] C = getC.Split(' ');
+
+            int[] timeC = C
+                .Select(str => int.Parse(str))
+                .ToArray();
+
+            for (int i = 0; i < timeC.Length; i++)
+            {
+                Debug.WriteLine(timeC[i].ToString());
+                int h1 = timeC[0];
+                int m1 = timeC[1];
+                int s1 = timeC[2];
+                int h2 = timeC[3];
+                int m2 = timeC[4];
+                int s2 = timeC[5];
+
+                int s = 0;
+                int m = 0;
+                int h = 0;
+
+                if (s2 - s1 < 0)
+                {
+                    s = s2 - s1 + 60;
+                    m2 -= 1;
+                }
+
+                if (m2 - m1 < 0)
+                {
+                    m = m2 - m1 + 60;
+                    h2 -= 1;
+                }
+
+                h = h2 - h1;
+
+                lblC.Text = h.ToString() + ' ' + m.ToString() + ' ' + s.ToString();
+            }
+        }
+    }
+}
