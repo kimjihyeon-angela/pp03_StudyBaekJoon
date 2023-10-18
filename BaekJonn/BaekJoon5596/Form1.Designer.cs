0d848eeb2995429e5747b72699c443a00a6d004d
kjh <angela9830@naver.com>
2023-10-18 14:11:31 +0900

    2023.10.18.

---
+++ BaekJonn/BaekJoon5596/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace BaekJoon5596
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxMinScore = new System.Windows.Forms.TextBox();
+            this.tbxManScore = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbxMinScore
+            // 
+            this.tbxMinScore.Location = new System.Drawing.Point(56, 23);
+            this.tbxMinScore.Name = "tbxMinScore";
+            this.tbxMinScore.Size = new System.Drawing.Size(180, 21);
+            this.tbxMinScore.TabIndex = 0;
+            // 
+            // tbxManScore
+            // 
+            this.tbxManScore.Location = new System.Drawing.Point(56, 50);
+            this.tbxManScore.Name = "tbxManScore";
+            this.tbxManScore.Size = new System.Drawing.Size(180, 21);
+            this.tbxManScore.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "민국 :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "만세 :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(242, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Location = new System.Drawing.Point(323, 35);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(100, 23);
+            this.lblResult.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 93);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxManScore);
+            this.Controls.Add(this.tbxMinScore);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxMinScore;
+        private System.Windows.Forms.MaskedTextBox tbxManScore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ BaekJonn/BaekJoon5596/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaekJoon5596
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string getMinScore = tbxMinScore.Text;
+            string getManScore = tbxManScore.Text;
+
+            int minTotalScore = 0;
+            int manTotalScore = 0;
+
+            string[] MinScore = getMinScore.Split(' ');
+
+            int[] MinScoreNumber = MinScore
+                .Select(str => int.Parse(str))
+                .ToArray();
+
+            string[] ManScore = getManScore.Split(' ');
+
+            int[] ManScoreNumber = ManScore
+                .Select(str => int.Parse(str))
+                .ToArray();
+
+            for (int i = 0; i < 4; i++)
+            {
+                minTotalScore = minTotalScore + MinScoreNumber[i];
+                manTotalScore = manTotalScore + ManScoreNumber[i];
+            }
+
+            if (manTotalScore > minTotalScore)
+            {
+                lblResult.Text = manTotalScore.ToString();
+            }
+            else
+            {
+                lblResult.Text = minTotalScore.ToString();
+            }
+
+        }
+    }
+}
